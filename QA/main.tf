8b58a5cc9d5d76d4df37b6623ef6b52800b8abe0
onkardevops7m <onkardevops7m@gmail.com>
2021-10-28 20:12:20 +0530

    update

--- QA/main.tf
+++ QA/main.tf
@@ -4,12 +4,12 @@
 #-------------------------------------------------------------------------
 
 resource "aws_instance" "webserver" {
-  ami                    = ami-013a129d325529d4d
-  instance_type          = t2.micro
-  key_name               = Windows
-  subnet_id              = subnet-efa85397
+  ami                    = "ami-013a129d325529d4d"
+  instance_type          = "t2.micro"
+  key_name               = "Windows"
+  subnet_id              = "subnet-efa85397"
   count                  = 1
-  vpc_security_group_ids = DevOps-Linux
+  vpc_security_group_ids = "DevOps-Linux"
   user_data              = file("script.sh")
   tags = {
     Name = "QA-Webserver"
