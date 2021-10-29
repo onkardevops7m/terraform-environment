ea86dd2b4b17044d46e72b982daad50ea8c8bc38
onkardevops7m <onkardevops7m@gmail.com>
2021-10-29 11:56:58 +0530

    update

--- QA/provider.tf
+++ QA/provider.tf
@@ -2,3 +2,11 @@ provider "aws" {
   version = "~> 2.0"
   region  = "us-west-2"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "environment-state-devops"
+    key    = "QA"
+    region = "us-west-2"
+  }
+}
